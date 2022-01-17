d589fbfb5a9b4930771c3dd6457cbfb9874bb713
Eemil Määttä <eemil.maatta@tuni.fi>
2022-01-17 15:21:58 +0200

    Tehtävät 4.13 ja 4.13

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -1,4 +1,6 @@
 const blogsRouter = require('express').Router()
+const { response } = require('express')
+const { request } = require('../app')
 const Blog = require('../models/blog')
 
 blogsRouter.get('/', async (request, response) => {
@@ -19,5 +21,19 @@ blogsRouter.post('/', async (request, response) => {
     const savedBlog = await blog.save()
     response.status(201).json(savedBlog.toJSON())
 })
+
+blogsRouter.delete('/:id', async (request, response) => {
+    await Blog.findByIdAndRemove(request.params.id)
+    response.status(204).end()
+})
+
+blogsRouter.put('/:id'), async (request, response) => {
+    const body = request.body
+    const blog = {
+        likes: body.likes
+    }
+    const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+    response.json(updatedBlog.toJSON())
+}
 
 module.exports = blogsRouter
