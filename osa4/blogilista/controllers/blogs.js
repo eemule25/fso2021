c705da44cb9572a342dd1e13680a8fa6a0fea710
Eemil Määttä <eemil.maatta@tuni.fi>
2022-01-29 17:29:23 +0200

    Tehtävät 4.15, 4.17-4.20

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -2,23 +2,37 @@ const blogsRouter = require('express').Router()
 const { response } = require('express')
 const { request } = require('../app')
 const Blog = require('../models/blog')
+const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
 blogsRouter.get('/', async (request, response) => {
-    const blogs = await Blog.find({})
+    const blogs = await Blog
+    .find({}).populate('user', {username: 1, name: 1})
     response.json(blogs.map(blog => blog.toJSON()))
   })
+
   
 blogsRouter.post('/', async (request, response) => {
     const body = request.body
+    const decodedToken = jwt.verify(request.token, process.env.SECRET)
+    if (!token || !decodedToken.id) {
+        return response.status(401).json({ error: 'token missing or invalid' })
+    }
+    const user = await User.findById(decodedToken.id)
+    console.log(body.userId)
 
     const blog = new Blog({
         title: body.title,
         author: body.author,
         url: body.url,
         likes: body.likes,
+        user: user._id
     })
 
     const savedBlog = await blog.save()
+    user.blogs = user.blogs.concat(savedBlog._id)
+    await user.save()
+
     response.status(201).json(savedBlog.toJSON())
 })
 
